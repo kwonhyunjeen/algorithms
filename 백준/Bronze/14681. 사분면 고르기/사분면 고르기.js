a57d0d75820975204b666dea50d0260369d18ac0
Hyunjin Kwon <157392752+kwonhyunjeen@users.noreply.github.com>
2025-03-26 14:57:11 +0900

    [Bronze V] Title: 사분면 고르기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+
+const x = parseInt(input[0]);
+const y = parseInt(input[1]);
+
+if (x > 0 && y > 0) {
+    console.log('1');
+} else if (x < 0 && y > 0) {
+    console.log('2');
+} else if (x < 0 && y < 0) {
+    console.log('3');
+} else if (x > 0 && y < 0) {
+    console.log('4');
+}
